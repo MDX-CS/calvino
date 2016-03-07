d52161da05f8cc6de6bc474ddb37cae0f0eb290c
Jaap Boender <jaapb@kerguelen.org>
2016-03-07 15:46:36 +0000

    Exercise can be added to database

--- calvino.rkt
+++ calvino.rkt
@@ -12,6 +12,9 @@
   (postgresql-connect #:user "jaap1" #:database "calvino")
   )
 
+(define save-exercise-query (prepare pgc "INSERT INTO exercises (description, inputs, code) \
+  VALUES ($1, $2, $3)"))
+
 (define (start request)
   (define (response-generator embed/url)
     (response/xexpr
@@ -21,6 +24,7 @@
         (h1 "Main menu")
         (ul
          (li (a [(href ,(embed/url main-page))] "Try some code"))
+         (li (a [(href ,(embed/url add-exercise))] "Add an exercise"))
          )
         )
        )
@@ -37,10 +41,10 @@
        (body
         (h1 "Evaluator")
         (p "Please enter some Racket code:")
-        (form ((action ,(embed/url code-page)))
+        (form [(action ,(embed/url code-page))]
               (table
-               (tr (td (textarea ((rows "10") (cols "80") (name "code")) "")))
-               (tr (td (input ((type "submit") (value "Evaluate!")))))
+               (tr (td (textarea [(rows "10") (cols "80") (name "code")] "")))
+               (tr (td (input [(type "submit") (value "Evaluate!")])))
                )
               )
         )
@@ -71,10 +75,59 @@
              [strange `(p ,(format "Okay, something really weird happened. (evaluation returned ~v)" strange))]
              )
           )
-         (p (a ((href ,(embed/url main-page))) "To main page"))
+         (p (a [(href ,(embed/url start))] "To main page"))
          )
        )
       )
     )
   (send/suspend/dispatch response-generator)
   )
+
+(define (add-exercise request)
+  (define (response-generator embed/url)
+    (response/xexpr
+     `(html
+       (head (title "Calvino"))
+       (body
+        (h1 "Add an exercise")
+        (form [(action ,(embed/url save-exercise))]
+              (table
+               (tr (td "The exercise description:"))
+               (tr (td (textarea [(rows "5") (cols "80") (name "description")])))
+               (tr (td "Sample inputs (separate arguments by a space, options by a newline):"))
+               (tr (td (textarea [(rows "10") (cols "80") (name "inputs")])))
+               (tr (td "Solution code:"))
+               (tr (td (textarea [(rows "20") (cols "80") (name "code")])))
+               (tr (td (input [(type "submit") (value "Save!")])))
+               )
+              )
+        )
+       )
+     )
+    )
+  (send/suspend/dispatch response-generator)
+  )
+
+(define (save-exercise request)
+  (define (response-generator embed/url)
+  (let
+      ([description (extract-binding/single 'description (request-bindings request))]
+       [inputs (extract-binding/single 'inputs (request-bindings request))]
+       [code (extract-binding/single 'code (request-bindings request))])
+    (query-exec pgc save-exercise-query description inputs code)
+    (response/xexpr
+     `(html
+       (head (title "Calvino"))
+       (body
+        (h1 "Done")
+        (p "Exercise saved.")
+        (p (a [(href ,(embed/url start))] "Back to main menu"))
+        )
+       )
+     )
+    )
+    )
+  (send/suspend/dispatch response-generator)
+  )
+      
+     
